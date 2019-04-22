e1ada50f756d351b11a00ec40b9c01bc85152337
afei <1640691253@qq.com>
2019-04-22 18:03:42 +0800

    1

--- js/common.js
+++ js/common.js
@@ -1,54 +1,19 @@
-//滚动
-// $(document).ready(function () {
-//     var box0 = $(".main_2"),v0 = 5; //这里添加滚动的对象和其速率
-// 	var box1 = $(".two"),v1 = 1;
-// 	Rin(box0,v0);
-// 	Rin(box1,v1); 
 
-// 		 function Rin($Box,v){//$Box移动的对象，v对象移动的速率
-//            var $Box_ul = $Box.find("ul"),
-//                $Box_li = $Box_ul.find("li"),
-//                $Box_li_span = $Box_li.find("span"),
-// 	           left = 0,
-// 	           s=0,
-// 	           timer;//定时器
 
-// 			      $Box_li.each(function(index){
-// 			       $($Box_li_span[index]).width($(this).width());//hover
-//                    s += $(this).outerWidth(true); //即要滚动的长度
-// 			     })
-			      
-// 		window.requestAnimationFrame = window.requestAnimationFrame||function(Tmove){return setTimeout(Tmove,1000/60)};
-// 	    window.cancelAnimationFrame = window.cancelAnimationFrame||clearTimeout;
-			      
-//                   if( s>=$Box.width()){//如果滚动长度超出Box长度即开始滚动，没有的话就不执行滚动
-//                            $Box_li.clone(true).appendTo($Box_ul);                       
-// 					       Tmove();
-// 						      function Tmove(){
-// 						           //运动是移动left  从0到-s;（个人习惯往左滚）
-// 						           left -= v;
-// 						           if(left <= -s){left = 0;$Box_ul.css("left",left)}else{ $Box_ul.css("left",left) }
-// 					                   timer = requestAnimationFrame(Tmove);   
-// 						      }
-// 					    $Box_ul.hover(function(){cancelAnimationFrame(timer)},function(){Tmove()})    
-// 		          }  
- 
-// 	} 	 	
-// }) 	
-//返回底部
 $("#to-top").click(function () {
         $('html , body').animate({scrollTop: 0}, 'slow');
   });
 //
-var mySwiper = new Swiper('.swiper_1', {
-	pagination: {
-	    el: '.swiper_1 .swiper-pagination',
-	    clickable :true,
-	},
-	navigation: {
-	  nextEl: '.swiper_1 .swiper-button-next',
-	  prevEl: '.swiper_1 .swiper-button-prev',
-	},
-});
 
-$(".main_w").slide({mainCell:"ul",autoPlay:true,effect:"leftMarquee",vis:5,interTime:1,});
+
+$(".headert h5 a").click(function () {
+    $(".mulu").show();
+});
+$(".mulu_a").click(function () {
+    $(".mulu").hide();
+})
+$(".wechat").click(function () {
+	$('.qrcode').fadeToggle();
+})
+
+
